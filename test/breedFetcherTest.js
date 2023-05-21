79e65d33f025a85a2adc1f4023892ba97e7ad063
Katherine Struthers <ktstruthers@gmail.com>
2023-05-21 18:49:26 -0500

    add unit tests

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,24 @@
+const fetchBreedDescription = require('../breedFetcher');
+const assert = require('chai').assert;
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  it('returns null for a non-existant breed, via callback', (done) => {
+    fetchBreedDescription('Husky', (err, desc) => {
+      assert.equal(null, desc);
+      done();
+    });
+  });
+});
